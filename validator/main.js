c11d8cade26e3290b52416d2a969c5c8ffd1cd35
hanwei <hanxiaowei92@163.com>
2017-08-30 22:14:45 +0800

    做的表单验证
    还没有做完，今天不舒服，早睡。明天继续做；

---
+++ validator/main.js
@@ -0,0 +1,18 @@
+
+// 选中页面中所有的input[data-rule]
+// 解析每个input的验证规则
+//验证
+var validator = new Validator("15",{
+    max:100,
+    min:18,
+});
+
+$(
+    function(){
+        "use strict";
+
+        window.Validator = function(val,rule){
+       val 
+        }
+    }
+)
